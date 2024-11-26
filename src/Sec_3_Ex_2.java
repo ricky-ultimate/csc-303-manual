6d8e73709388b06d981f846776b18b2792c9cf18
Ricky Ultimate <rickyogigi@gmail.com>
2024-11-26 07:31:05 +0100

    feat: Section 3 - Exercise 2

---
+++ src/Sec_3_Ex_2.java
@@ -0,0 +1,17 @@
+
+/* Java program to perform relational operations on the
+   integers 40 and 20
+   */
+public class Sec_3_Ex_2 {
+    public static void main(String[] args){
+        int a = 40;
+        int b = 20;
+
+        System.out.println("a == b : " + (a == b) );
+        System.out.println("a != b : " + (a != b) );
+        System.out.println("a > b : "+ (a > b) );
+        System.out.println("a < b : " + (a < b) );
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+    }
+}
