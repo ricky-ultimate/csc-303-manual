c7432870c4e760e34f99351c251b13ccc276b213
Ricky Ultimate <rickyogigi@gmail.com>
2024-11-26 07:46:00 +0100

    feat: Section 3 - Exercise 3

---
+++ src/Sec_3_Ex_3.java
@@ -0,0 +1,13 @@
+/* Java program to demonstrate some
+   logical operations
+ */
+public class Sec_3_Ex_3 {
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b : " + (a && b));
+        System.out.println("a || b : " + (a || b));
+        System.out.println("!a: " + !a + "\n!b: " + !b);
+    }
+}
